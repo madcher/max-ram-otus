1bede06e1f8ee90d6ab94c6c01533ab3ab37fe7b
Maksim.Rameev <madcher@yandex.ru>
2020-12-26 20:56:18 +0300

    lesson 1 second variant

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 
-const getTree = (obj, count = 0) => {
-    console.log('  '.repeat(count) + '└─' + obj.name)
-    if (obj.items && obj.items.length) {
-        count += 1;
-        obj.items.forEach(item => {
-            return getTree(item, count);
-        })
-    } 
-}
-
+var fs = require('fs');
+var path = require('path');
+// test object
 const testObj = {
     "name": 1,
     "items": [{
@@ -20,7 +13,34 @@ const testObj = {
         }
     ]
 };
+// get tree from object
+const getTree = (obj, count = 0) => {
+    console.log('  '.repeat(count) + '└─' + obj.name)
+    if (obj.items && obj.items.length) {
+        count += 1;
+        obj.items.forEach(item => {
+            return getTree(item, count);
+        })
+    } 
+}
+// get files and fiolders tree
+var getFiles = function (dir, depth = 2, count = 0){
+    console.log('  '.repeat(count) + '└─' + dir);
+    if (fs.statSync(dir).isDirectory()) {
+        count += 1;
+        let files = fs.readdirSync(dir);
+        files.forEach(file => {
+            var name = dir + '/' + file;
+            if (count <= depth) {
+                return getFiles(name, depth, count);
+            }
+        })
+    }
+    return;
+};
 
 getTree(testObj);
+getFiles('./');
 
-module.exports = getTree;
+module.exports.getTree;
+module.exports.getFiles;
